8f9597fec92bc2282b49612a7548cc09da3747a2
usertime2022 <tynchtyk7@mail.ru>
2022-06-01 16:43:22 +0600

    2

--- src/com/company/Kundor.java
+++ src/com/company/Kundor.java
@@ -1,11 +1,21 @@
 package com.company;
 
 public enum Kundor {
-    ДУЙШОМБУ,
-    ШЕЙШЕМБИ,
-    ШАРШЕМБИ,
-    БЕЙШЕМБИ,
-    ЖУМА,
-    ИШЕМБИ,
-    ЖЕКШЕМБИ;
+    ДУЙШОМБУ("Дуйшомбу"),
+    ШЕЙШЕМБИ("Шейшемби"),
+    ШАРШЕМБИ("Шаршемби"),
+    БЕЙШЕМБИ("Бейшемби"),
+    ЖУМА("Жума"),
+    ИШЕМБИ("Ишемби"),
+    ЖЕКШЕМБИ("Жекшемби");
+
+    private String name;
+
+    Kundor(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -11,25 +11,32 @@ public class Main {
             String soz = scanner.nextLine().toUpperCase();
             switch (soz) {
                 case ("ДУЙШОМБУ"):
-                    System.out.println(Kundor.ДУЙШОМБУ + " куну java тилин уйроном!");
+                    System.out.println(Kundor.ДУЙШОМБУ.getName() + " куну java тилин уйроном!");
+                    System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~");
                     break;
                 case ("ШЕЙШЕМБИ"):
-                    System.out.println(Kundor.ШЕЙШЕМБИ + " куну Англис тилин уйроном!");
+                    System.out.println(Kundor.ШЕЙШЕМБИ.getName() + " куну Англис тилин уйроном!");
+                    System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~");
                     break;
                 case ("ШАРШЕМБИ"):
-                    System.out.println(Kundor.ШАРШЕМБИ + " куну Soft skills уйроном!");
+                    System.out.println(Kundor.ШАРШЕМБИ.getName() + " куну Soft skills уйроном!");
+                    System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~");
                     break;
                 case ("БЕЙШЕМБИ"):
-                    System.out.println(Kundor.БЕЙШЕМБИ + " куну java тилин практика кылам!");
+                    System.out.println(Kundor.БЕЙШЕМБИ.getName() + " куну java тилин практика кылам!");
+                    System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~");
                     break;
                 case ("ЖУМА"):
-                    System.out.println(Kundor.ЖУМА + " куну Англис тилин практика кылам!");
+                    System.out.println(Kundor.ЖУМА.getName() + " куну Англис тилин практика кылам!");
+                    System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~");
                     break;
                 case ("ИШЕМБИ"):
-                    System.out.println(Kundor.ИШЕМБИ + " жумуш кылам!");
+                    System.out.println(Kundor.ИШЕМБИ.getName() + " жумуш кылам!");
+                    System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~");
                     break;
                 case ("ЖЕКШЕМБИ"):
-                    System.out.println(Kundor.ЖЕКШЕМБИ + " эс алам!");
+                    System.out.println(Kundor.ЖЕКШЕМБИ.getName() + " эс алам!");
+                    System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~");
                     break;
                 default:
                     System.out.println("Ката жаздыныз!");
